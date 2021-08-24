93fd95d9c6a891f947b2a1aeb8e1c300783a4955
Ruben Ariza <Ruben Ariza@DESKTOP-MH7ROPR>
2021-08-24 14:46:30 -0500

    Agregar archivos de proyecto.

---
+++ Taller1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller1
+{
+    class Ejercicio
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, promedio;
+            Console.WriteLine("Digite nota 1: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite nota 2: ");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite nota 3: ");
+            nota3 = double.Parse(Console.ReadLine());
+            promedio = (nota1 + nota2 + nota3) / 3;
+            Console.WriteLine("El promedio es: " + promedio);
+            if (promedio >= 3)
+            {
+                Console.WriteLine("El estudiante ganó la asignatura");
+            }
+            else
+            {
+                Console.WriteLine("El estudiante perdió la asignatura");
+            }
+        }
+    }
+}using System;
+
+        
+    
+
